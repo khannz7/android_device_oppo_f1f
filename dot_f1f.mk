1f4d3bde92f661d7838202ca2f0a6b8996e00bda
khannz7 <thekhannz@gmail.com>
2019-04-04 13:58:48 +0000

    f1f dotify

---
+++ dot_f1f.mk
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit framework first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from f1f device
+$(call inherit-product, device/oppo/f1f/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
+
+PRODUCT_NAME := dot_f1f
+PRODUCT_DEVICE := f1f
+PRODUCT_BRAND := OPPO
+PRODUCT_MODEL := F1f
+PRODUCT_MANUFACTURER := OPPO
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8916_64-user 6.0.1 MMB29M eng.root.20160628.174309 release-keys" \
+    TARGET_DEVICE="F1f"
+
+BUILD_FINGERPRINT=OPPO/F1f/F1f:6.0.1/MMB29M/1456475076:user/release-keys
